17d304000617d684461d4f4d9c7b555d63127bdc
Anh Do Thi Ngoc <anh.dothingoc@imac-0021.pyco.vn>
2019-05-22 15:46:03 +0700

    First commit

--- src/db.js
+++ src/db.js
@@ -1,5 +1,13 @@
+
+require('dotenv').config();
 const mongoose  = require("mongoose");
-const url = "mongodb://localhost:27017/user";
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const host = process.env.DP_HOST;
+const url = process.env.MONGOLAB_URI;
+console.log(username, password, host)
+// const url = `mongodb+srv://${username}:${password}${host}`;
+// const d ="mongodb+srv://NHI-NGUYEN_97:nhinguyen040197%40@cluster0-odomf.mongodb.net/order?retryWrites=true";
 mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true
